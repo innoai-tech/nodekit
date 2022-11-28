ffcf0cb4cce502a172db1430659a76a85fc49146
Morlay <morlay.null@gmail.com>
2022-11-28 16:03:28 +0800

    feat(gents): options fix

--- nodedevpkg/gents/src/__tests__/index.spec.ts
+++ nodedevpkg/gents/src/__tests__/index.spec.ts
@@ -1,22 +1,21 @@
-import { describe, it } from "vitest";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
+import {describe, it} from "vitest";
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
 
-import { generateClient } from "..";
+import {generateClient} from "..";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 describe("#generateClient", () => {
-  it("gen", async () => {
-    await generateClient({
-      id: "example",
-      uri: `files://${join(__dirname, "example/openapi.json")}`,
-      outDir: join(__dirname, "client"),
-      requestCreator: {
-        expose: "createRequest",
-        importPath: "./client",
-      },
-      force: true,
+    it("gen", async () => {
+        await generateClient({
+            id: "example",
+            uri: `files://${join(__dirname, "example/openapi.json")}`,
+            outDir: join(__dirname, "client"),
+            requestCreator: {
+                expose: "createRequest",
+                importPath: "./client",
+            },
+        });
     });
-  });
 });
--- nodedevpkg/gents/src/index.ts
+++ nodedevpkg/gents/src/index.ts
@@ -1,58 +1,64 @@
-import { readFile } from "fs/promises";
+import {readFile} from "fs/promises";
 import fetch from "node-fetch";
-import { genClientFromOpenAPI, RequestCreator } from "./gen";
-import { join } from "path";
-import { camelCase } from "@innoai-tech/lodash";
-import type { AppConfig, AppConfigMetadata } from "@innoai-tech/config";
+import {genClientFromOpenAPI, RequestCreator} from "./gen";
+import {join} from "path";
+import {camelCase} from "@innoai-tech/lodash";
+import type {AppConfig, AppConfigMetadata} from "@innoai-tech/config";
 
-export interface Options {
-  id: string;
-  uri: string;
-  outDir: string;
-  requestCreator: RequestCreator;
-  force: boolean;
+export interface GenerateOptions {
+    requestCreator: RequestCreator;
+    includesRawOpenAPI?: boolean;
+}
+
+export interface Options extends GenerateOptions {
+    id: string;
+    uri: string;
+    outDir: string;
 }
 
 const loadOpenAPI = async (uri: string): Promise<any> => {
-  const u = new URL(uri);
+    const u = new URL(uri);
 
-  if (u.protocol === "files:") {
-    const b = await readFile(u.pathname);
-    return JSON.parse(`${b}`);
-  }
+    if (u.protocol === "files:") {
+        const b = await readFile(u.pathname);
+        return JSON.parse(`${b}`);
+    }
 
-  const res = await fetch(u.toString());
-  return await res.json();
+    const res = await fetch(u.toString());
+    return await res.json();
 };
 
 export const generateClient = async (opt: Options) => {
-  const openapi = await loadOpenAPI(opt.uri);
+    const openapi = await loadOpenAPI(opt.uri);
 
-  const f = genClientFromOpenAPI(opt.id, openapi, opt.requestCreator);
+    const f = genClientFromOpenAPI(opt.id, openapi, opt.requestCreator);
 
-  return f.sync(join(opt.outDir, `${camelCase(opt.id)}.ts`));
+    if (opt.includesRawOpenAPI) {
+        f.const("RawOpenAPI", JSON.stringify(openapi, null, 2))
+    }
+
+    return f.sync(join(opt.outDir, `${camelCase(opt.id)}.ts`));
 };
 
 export const generateClients = async (
-  outDir: string,
-  c: AppConfig & AppConfigMetadata,
-  requestCreator: Options["requestCreator"]
+    outDir: string,
+    c: AppConfig & AppConfigMetadata,
+    options: GenerateOptions
 ) => {
-  for (const k in c.config) {
-    if (c.metadata[k] && c.metadata[k]!.api) {
-      const api = c.metadata[k]!.api!;
-      const id = api.id!;
-      const openapi = `${c.config[k]}${api.openapi}`;
+    for (const k in c.config) {
+        if (c.metadata[k] && c.metadata[k]!.api) {
+            const api = c.metadata[k]!.api!;
+            const id = api.id!;
+            const openapi = `${c.config[k]}${api.openapi}`;
 
-      console.log(`generate client ${id} from ${openapi}`);
+            console.log(`generate client ${id} from ${openapi}`);
 
-      await generateClient({
-        id,
-        uri: openapi,
-        outDir,
-        requestCreator: requestCreator,
-        force: true,
-      });
+            await generateClient({
+                ...options,
+                id,
+                uri: openapi,
+                outDir,
+            });
+        }
     }
-  }
 };
