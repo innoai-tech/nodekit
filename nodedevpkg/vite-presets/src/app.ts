c9b0f40aaaae9a0fb1048383880d39b65e4811fa
Morlay <morlay.null@gmail.com>
2023-03-08 10:37:25 +0800

    feat(vite-presets): use vite-tsconfig-paths

--- nodedevpkg/vite-presets/src/app.ts
+++ nodedevpkg/vite-presets/src/app.ts
@@ -1,46 +1,24 @@
-import {join} from "path";
-import type {PluginOption} from "vite";
-import {readFile} from "fs/promises";
-import {existsSync} from "fs";
-
-const resolveWorkspaceAlias = async (root: string) => {
-    const pkgJson = JSON.parse(String(await readFile(join(root, "package.json"))));
-
-    const aliases: Record<string, string> = {}
-
-    if (pkgJson.dependencies) {
-        for (const pkg in pkgJson.dependencies) {
-            if (pkgJson.dependencies[pkg].startsWith("workspace:")) {
-                const aliasTo = join(root, `node_modules/${pkg}/src/index.ts`)
-                if (existsSync(aliasTo)) {
-                    aliases[pkg] = aliasTo;
-                }
-            }
-        }
-    }
-
-    return aliases
-};
+import { join } from "path";
+import type { PluginOption } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export const app = (
-    appName: string,
-    {enableBaseHref}: { enableBaseHref?: boolean } = {}
-): PluginOption => {
-    return {
-        name: "vite-presets/app",
-        async config(c) {
-            c.root = "./src";
-            c.base = enableBaseHref ? `/${appName}/` : "/";
-            c.build = c.build ?? {};
-            c.build.outDir = join(process.cwd(), "./dist");
-            c.build.emptyOutDir = true;
+  appName: string,
+  { enableBaseHref }: { enableBaseHref?: boolean } = {}
+): PluginOption[] => {
+  return [
+    {
+      name: "vite-presets/app",
+      async config(c) {
+        c.root = "./src";
+        c.base = enableBaseHref ? `/${appName}/` : "/";
+        c.build = c.build ?? {};
+        c.build.outDir = join(process.cwd(), "./dist");
+        c.build.emptyOutDir = true;
 
-            c.resolve = c.resolve ?? {}
-
-            c.resolve.alias = {
-                ...(c.resolve.alias || {}),
-                ...(await resolveWorkspaceAlias(process.cwd())),
-            };
-        },
-    };
+        c.resolve = c.resolve ?? {};
+      }
+    },
+    tsconfigPaths()
+  ];
 };
