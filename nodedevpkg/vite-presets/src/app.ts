4731e1b8b8b77c8036b44e47133294fe53dbae11
Morlay <morlay.null@gmail.com>
2023-03-09 19:27:40 +0800

    fix(vite-presets): filenames should have prefix to avoid filename starts with _

--- nodedevpkg/vite-presets/src/app.ts
+++ nodedevpkg/vite-presets/src/app.ts
@@ -16,6 +16,14 @@ export const app = (
         c.build.outDir = join(process.cwd(), "./dist");
         c.build.emptyOutDir = true;
 
+        c.build.rollupOptions = c.build.rollupOptions ?? {};
+        // to avoid some filename starts with _
+        c.build.rollupOptions.output = {
+          assetFileNames: "asset-[name]-[hash].js",
+          entryFileNames: "entry-[name]-[hash].js",
+          chunkFileNames: "chunk-[name]-[hash].js"
+        };
+
         c.resolve = c.resolve ?? {};
       }
     },
