8a6dd7087926e2945791ac5af5998ddd995c3ca4
Morlay <morlay.null@gmail.com>
2023-03-09 12:45:12 +0800

    fix(reactutil): StateSubject should only next when value changed

--- nodepkg/reactutil/src/observable/StateSubject.tsx
+++ nodepkg/reactutil/src/observable/StateSubject.tsx
@@ -1,18 +1,21 @@
-import {isFunction} from "@innoai-tech/lodash";
-import {BehaviorSubject} from "rxjs";
-import {useMemoObservable} from "./useMemoObservable";
+import { isFunction } from "@innoai-tech/lodash";
+import { BehaviorSubject } from "rxjs";
+import { useMemoObservable } from "./useMemoObservable";
 
 export class StateSubject<T> extends BehaviorSubject<T> {
-    static override create<T>(initialValue: T | (() => T)) {
-        return new StateSubject<T>(isFunction(initialValue) ? initialValue() : initialValue)
-    }
+  static override create<T>(initialValue: T | (() => T)) {
+    return new StateSubject<T>(isFunction(initialValue) ? initialValue() : initialValue);
+  }
 
-    override next = (value: T | ((value: T) => T)) => {
-        super.next(isFunction(value) ? value(super.value) : value);
+  override next = (value: T | ((value: T) => T)) => {
+    const nextValue = isFunction(value) ? value(super.value) : value;
+    if (!Object.is(nextValue, this.value)) {
+      super.next(nextValue);
     }
+  };
 }
 
 export const useStateSubject = <T extends any>(initialValue: T | (() => T)) => {
-    return useMemoObservable(() => StateSubject.create<T>(initialValue));
+  return useMemoObservable(() => StateSubject.create<T>(initialValue));
 };
 
