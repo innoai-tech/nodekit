bd7a63b67eef6817970b9633ab204feb1ed45c3d
Morlay <morlay.null@gmail.com>
2024-08-13 11:39:50 +0800

    config: should load base href

--- nodepkg/config/src/config.tsx
+++ nodepkg/config/src/config.tsx
@@ -3,58 +3,68 @@ import { parse } from "./configvalue";
 export type BaseConfig = { name: string; env: string; version: string };
 
 const getWebAppConfigValue = (key: string) => {
-	return (
-		(globalThis as any).document
-			?.querySelector(`meta[name="webapp:${key}"]`)
-			?.getAttribute("content") || ""
-	);
+  return (
+    (globalThis as any).document
+      ?.querySelector(`meta[name="webapp:${key}"]`)
+      ?.getAttribute("content") || ""
+  );
+};
+
+
+const getAppBaseHref = () => {
+  return (
+    (globalThis as any).document
+      ?.querySelector("base")
+      ?.getAttribute("href") || "/"
+  );
 };
 
 export const confLoader = <TKeys extends string>() => {
-	const base = parse(getWebAppConfigValue("base")) as BaseConfig;
-	const config = parse(getWebAppConfigValue("config")) as {
-		[key in TKeys]: string;
-	};
+  const base = parse(getWebAppConfigValue("base")) as BaseConfig;
+  const config = parse(getWebAppConfigValue("config")) as {
+    [key in TKeys]: string;
+  };
 
-	return () => {
-		return {
-			...base,
-			...config,
-		};
-	};
+  return () => {
+    return {
+      ...base,
+      ...config,
+      baseHref: getAppBaseHref()
+    };
+  };
 };
 
 export interface AppContext {
-	env: string;
-	feature: string;
+  env: string;
+  feature: string;
 }
 
 export type ConfigBuilder = (ctx: AppContext) => string;
 
 export interface AppConfig {
-	name: string;
-	group: string;
-	config: { [n: string]: ConfigBuilder | string };
-	manifest: { [k: string]: string };
+  name: string;
+  group: string;
+  config: { [n: string]: ConfigBuilder | string };
+  manifest: { [k: string]: string };
 }
 
 export interface AppConfigMetadata {
-	metadata: {
-		[n: string]: {
-			api?: APIMetadata;
-		};
-	};
+  metadata: {
+    [n: string]: {
+      api?: APIMetadata;
+    };
+  };
 }
 
 export interface APIMetadata {
-	id?: string;
-	openapi?: string;
+  id?: string;
+  openapi?: string;
 }
 
 export const api = (opt: APIMetadata = {}) => {
-	return <T extends Function>(target: T) => {
-		return Object.assign(target, {
-			api: opt,
-		});
-	};
+  return <T extends Function>(target: T) => {
+    return Object.assign(target, {
+      api: opt
+    });
+  };
 };
