cf4a3720b8eea815b9324d18a6299ecd200a05f6
Morlay <morlay.null@gmail.com>
2025-05-16 13:50:17 +0800

    feat(fetcher): add build for building request config as something like sse

--- nodepkg/fetcher/src/__tests__/RequestSubject.spec.ts
+++ nodepkg/fetcher/src/__tests__/RequestSubject.spec.ts
@@ -2,55 +2,57 @@ import { describe, expect, it } from "bun:test";
 import { createRequestSubject } from "../subject";
 
 describe("RequestSubject", () => {
-	describe("#success", () => {
-		const rs$ = createRequestSubject(
-			((input: any) => ({ body: input })) as any,
-			{
-				request: (c) => Promise.resolve(c.body),
-				toHref: () => "",
-			},
-		);
+  describe("#success", () => {
+    const rs$ = createRequestSubject(
+      ((input: any) => ({ body: input })) as any,
+      {
+        build: (c) => c,
+        request: (c) => Promise.resolve(c.body),
+        toHref: () => ""
+      }
+    );
 
-		const rets: any[] = [];
+    const rets: any[] = [];
 
-		rs$.subscribe((resp) => rets.push(resp));
+    rs$.subscribe((resp) => rets.push(resp));
 
-		it("request 1", async () => {
-			rs$.next(1);
-			await Promise.resolve();
-			expect(rets).toEqual([1]);
-		});
+    it("request 1", async () => {
+      rs$.next(1);
+      await Promise.resolve();
+      expect(rets).toEqual([1]);
+    });
 
-		it("request 2", async () => {
-			rs$.next(2);
-			await Promise.resolve();
-			expect(rets).toEqual([1, 2]);
-		});
-	});
+    it("request 2", async () => {
+      rs$.next(2);
+      await Promise.resolve();
+      expect(rets).toEqual([1, 2]);
+    });
+  });
 
-	describe("#failed", () => {
-		const rs$ = createRequestSubject(
-			((input: any) => ({ body: input })) as any,
-			{
-				request: (_) => Promise.reject("err"),
-				toHref: () => "",
-			},
-		);
+  describe("#failed", () => {
+    const rs$ = createRequestSubject(
+      ((input: any) => ({ body: input })) as any,
+      {
+        build: (c) => c,
+        request: (_) => Promise.reject("err"),
+        toHref: () => ""
+      }
+    );
 
-		const rets: any[] = [];
+    const rets: any[] = [];
 
-		rs$.error$.subscribe((resp) => rets.push(resp));
+    rs$.error$.subscribe((resp) => rets.push(resp));
 
-		it("request 1", async () => {
-			rs$.next({ x: 1 });
-			await Promise.resolve();
-			expect(rets).toEqual(["err"]);
-		});
+    it("request 1", async () => {
+      rs$.next({ x: 1 });
+      await Promise.resolve();
+      expect(rets).toEqual(["err"]);
+    });
 
-		it("request 2", async () => {
-			rs$.next({ x: 1 });
-			await Promise.resolve();
-			expect(rets).toEqual(["err", "err"]);
-		});
-	});
+    it("request 2", async () => {
+      rs$.next({ x: 1 });
+      await Promise.resolve();
+      expect(rets).toEqual(["err", "err"]);
+    });
+  });
 });
