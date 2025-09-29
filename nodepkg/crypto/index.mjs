d5f99df3184e28be3d77c97820872097861d14b4
Morlay <morlay.null@gmail.com>
2025-09-29 13:43:54 +0800

    fix(crypto): migrate from deprecated params

--- nodepkg/crypto/index.mjs
+++ nodepkg/crypto/index.mjs
@@ -12,31 +12,31 @@ import init, {
 import wasmURL from "./pkg/crypto_bg.wasm?url";
 
 export const generate_aes_256_key = (mode = "CFB") => {
-  return init(wasmURL).then(() => _generate_aes_256_key(mode));
+  return init({ module_or_path: wasmURL }).then(() => _generate_aes_256_key(mode));
 };
 
 export const generate_chacha20_key = () => {
-  return init(wasmURL).then(() => _generate_chacha20_key(""));
+  return init({ module_or_path: wasmURL }).then(() => _generate_chacha20_key(""));
 };
 
 export const rsa_oaep_encrypt = (payload, key) => {
-  return init(wasmURL).then(() => _rsa_oaep_encrypt(payload, JSON.stringify(key)));
+  return init({ module_or_path: wasmURL }).then(() => _rsa_oaep_encrypt(payload, JSON.stringify(key)));
 };
 
 export const aes_256_decrypt = (payload, key) => {
-  return init(wasmURL).then(() => _aes_256_decrypt(payload, key));
+  return init({ module_or_path: wasmURL }).then(() => _aes_256_decrypt(payload, key));
 };
 
 export const aes_256_encrypt = (payload, key) => {
-  return init(wasmURL).then(() => _aes_256_encrypt(payload, key));
+  return init({ module_or_path: wasmURL }).then(() => _aes_256_encrypt(payload, key));
 };
 
 export const chacha20_encrypt = (payload, key) => {
-  return init(wasmURL).then(() => _chacha20_encrypt(payload, key));
+  return init({ module_or_path: wasmURL }).then(() => _chacha20_encrypt(payload, key));
 };
 
 export const chacha20_decrypt = (payload, key) => {
-  return init(wasmURL).then(() => _chacha20_decrypt(payload, key));
+  return init({ module_or_path: wasmURL }).then(() => _chacha20_decrypt(payload, key));
 };
 
 
